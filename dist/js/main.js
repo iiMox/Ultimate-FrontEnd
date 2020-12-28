02318b74115135ba450ce73f927e18243e24fa67
iiMox <iimox04@gmail.com>
2020-12-28 11:45:45 +0100

    Lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,4 +19,68 @@ $(function () {
 
     });
 
+    // Toggle Full Screen
+
+    $(".toggle-fullscreen").on("click", function () {
+
+        $(this).toggleClass("full-screen");
+
+        if ($(this).hasClass("full-screen")) {
+
+            openFullscreen();
+
+        } else {
+
+            closeFullscreen();
+
+        }
+
+    })
+
 })
+
+var elem = document.documentElement;
+
+function openFullscreen() {
+
+    if (elem.requestFullscreen) {
+
+        elem.requestFullscreen();
+
+    } else if (elem.mozRequestFullscreen) {
+
+        elem.mozRequestFullscreen();
+
+    } else if (elem.webkitRequestFullscreen) {
+
+        elem.webkitRequestFullscreen();
+
+    } else if (elem.msReuestFullscreen) {
+
+        elem.msReuestFullscreen();
+
+    }
+
+}
+
+function closeFullscreen() {
+
+    if (document.exitFullscreen) {
+
+        document.exitFullscreen();
+
+    } else if (document.mozcancelFullscreen) {
+
+        document.mozcancelFullscreen();
+
+    } else if (document.webkitExitFullscreen) {
+
+        document.webkitExitFullscreen();
+
+    } else if (document.msExitFullscreen) {
+
+        document.msExitFullscreen();
+
+    }
+
+}
